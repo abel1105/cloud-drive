93397745df59c80cdf8d594b880a3dc638103761
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-01 13:21:16 +0000

    Deploy abel1105/local-storage-drive to github.com/abel1105/local-storage-drive.git:gh-pages

---
+++ precache-manifest.ae780db3c35098be42dfcf9f2ce185d0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5e12ffcd6090a1ede8a346f63a5f40",
+    "url": "/local-storage-drive/index.html"
+  },
+  {
+    "revision": "eff5bf1a7cb4efeb6eca",
+    "url": "/local-storage-drive/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "dfcfd57c2412f6c7fb1e",
+    "url": "/local-storage-drive/static/css/main.b79b7104.chunk.css"
+  },
+  {
+    "revision": "eff5bf1a7cb4efeb6eca",
+    "url": "/local-storage-drive/static/js/2.3f9c7396.chunk.js"
+  },
+  {
+    "revision": "dfcfd57c2412f6c7fb1e",
+    "url": "/local-storage-drive/static/js/main.c70c5cc2.chunk.js"
+  },
+  {
+    "revision": "98bd7ba76256e7244d09",
+    "url": "/local-storage-drive/static/js/runtime~main.840bf708.js"
+  },
+  {
+    "revision": "4d8aa304cb7f7bfda97d77cb66c2c51c",
+    "url": "/local-storage-drive/static/media/avatar.4d8aa304.png"
+  },
+  {
+    "revision": "ee43af90ebf678fb99882d020a79278d",
+    "url": "/local-storage-drive/static/media/doc.ee43af90.svg"
+  },
+  {
+    "revision": "573df713a957068c3821a3c51fd0bd91",
+    "url": "/local-storage-drive/static/media/img.573df713.svg"
+  },
+  {
+    "revision": "b01227c360b4063db11dc5a97bb9c61f",
+    "url": "/local-storage-drive/static/media/other.b01227c3.svg"
+  },
+  {
+    "revision": "ee5ba3426be31e933f32c7c3e78ad023",
+    "url": "/local-storage-drive/static/media/pdf.ee5ba342.svg"
+  },
+  {
+    "revision": "feed0a56518bdc7742976be25ec1c2d3",
+    "url": "/local-storage-drive/static/media/search.feed0a56.svg"
+  }
+]);
